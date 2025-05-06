ba6291c918f074a8b21baa739fd17b9ecc09ca87
Plousieleex <a-xy.cnbrk@hotmail.com>
2025-05-06 03:08:50 +0300

    Updated.

--- backend/src/modules/user/user.routes.js
+++ backend/src/modules/user/user.routes.js
@@ -1,14 +1,3 @@
-/* const express = require('express');
-const userController = require('./user.controller');
-const authMiddleware = require('../auth/auth.middleware');
-
-const router = express.Router();
-
-router
-  .route('/userprofile')
-  .post(authMiddleware.protect, userController.createUserProfile);
-
-module.exports = router; */
 import { Router } from 'express';
 import userController from './user.controller.js';
 import authMiddleware from '../auth/auth.middleware.js';
