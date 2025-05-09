97851aa72ebd33428ba51761a8139c5427af7ae1
Plousieleex <a-xy.cnbrk@hotmail.com>
2025-05-09 15:40:47 +0300

    getWorkout, getAllWorkouts, joinToWorkout, getAllWorkoutsCreatedByUser,getAllWorkoutsParticipate, updateWorkout function controllers implemented.

--- backend/src/modules/workout/workout.controller.js
+++ backend/src/modules/workout/workout.controller.js
@@ -41,24 +41,77 @@ export const getWorkout = handleAsync(async (req, res, next) => {
 
 export const getAllWorkouts = handleAsync(async (req, res, next) => {
   // Get all workouts created or participated
+  const workouts = await workoutService.getAllWorkouts();
+
+  res.status(201).json({
+    status: 'success',
+    results: workouts.length,
+    data: workouts,
+  });
 });
 
 export const getAllWorkoutsCreatedByUser = handleAsync(
   async (req, res, next) => {
     // Get all workouts created by user
+    const userID = req.user.id;
+
+    const createdWorkouts = await workoutService.getAllWorkoutsCreatedByUser(
+      userID,
+    );
+
+    res.status(200).json({
+      status: 'success',
+      data: createdWorkouts,
+    });
   },
 );
 
 export const getAllWorkoutsParticipate = handleAsync(async (req, res, next) => {
   // Get all workouts user participated
+  const userID = req.user.id;
+
+  const workouts = await workoutService.getAllWorkoutsParticipate(userID);
+
+  res.status(200).json({
+    status: 'success',
+    results: workouts.length,
+    data: workouts,
+  });
 });
 
 export const updateWorkout = handleAsync(async (req, res, next) => {
   // Update Workout
+  const userID = req.user.id;
+  const workoutID = Number(req.params.id);
+
+  const workoutData = req.body;
+
+  const updatedWorkout = await workoutService.updateWorkout(
+    userID,
+    workoutID,
+    workoutData,
+  );
+
+  res.status(200).json({
+    status: 'success',
+    data: updatedWorkout,
+  });
 });
 
 export const deleteWorkout = handleAsync(async (req, res, next) => {
   // Delete Workout
+});
+
+export const joinToWorkout = handleAsync(async (req, res, next) => {
+  const userID = req.user.id;
+  const workoutID = Number(req.params.id);
+
+  const joinedWorkout = await workoutService.joinToWorkout(userID, workoutID);
+
+  res.status(200).json({
+    status: 'success',
+    data: joinedWorkout,
+  });
 });
 
 export default {
@@ -69,4 +122,5 @@ export default {
   getAllWorkoutsParticipate,
   updateWorkout,
   deleteWorkout,
+  joinToWorkout,
 };
